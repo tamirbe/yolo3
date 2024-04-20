d4c125ed9379e702bb460ec2e7e9ec69a5593efc
tamir beneden <118120170+tamirbe@users.noreply.github.com>
2024-04-20 21:52:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+import cv2
+
+
+# # Load YOLO
+
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# Load image
+img = cv2.imread("lung_image.jpg")
+img = cv2.resize(img, None, fx=0.4, fy=0.4)
+height, width, channels = img.shape
+
+# Detecting objects
+blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+net.setInput(blob)
+outs = net.forward(output_layers)
+
+# Showing information on the screen
+class_ids = []
+confidences = []
+boxes = []
+for out in outs:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.5:  # Confidence threshold
+            # Object detected
+            center_x = int(detection[0] * width)
+            center_y = int(detection[1] * height)
+            w = int(detection[2] * width)
+            h = int(detection[3] * height)
+            # Rectangle coordinates
+            x = int(center_x - w / 2)
+            y = int(center_y - h / 2)
+            boxes.append([x, y, w, h])
+            confidences.append(float(confidence))
+            class_ids.append(class_id)
+
+
+plt.imshow(image, cmap='gray')
+plt.axis('off')
+plt.show()
+total_storage_used = 0
+for root, dirs, files in os.walk('.'):
+    for file in files:
+        total_storage_used += os.path.getsize(os.path.join(root, file))
+
+print("Total storage used : %s bytes" % total_storage_used)
+
+
+print("--- CPU Time: %s seconds ---" % (time.process_time() - start_cpu_time))
